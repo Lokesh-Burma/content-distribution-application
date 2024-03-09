33c4d98fd6632f6d8c97cda5f0ca57026c0f3992
lokeshburma <lokesh.burma@outlook.com>
2024-03-09 11:26:06 +0530

    added condition to check the current ip address before forwarding to request

--- server.py
+++ server.py
@@ -9,7 +9,11 @@ import os
 import socket
 
 # Define the IP address and port of the other systems running the server.py script
-other_systems = [("192.168.1.101", 12346), ("192.168.1.102", 12347)]
+other_systems = [
+    ("172.31.8.60", 12345),
+    ("172.31.6.92", 12345),
+    ("172.31.2.235", 12345)
+]
 
 
 def save_file(file_name, file_content):
@@ -28,22 +32,24 @@ def handle_client_request(client_socket, file_name, server_info):
             client_socket.send(file_content_with_info.encode())
     except FileNotFoundError:
         # Forward the file request to other systems if not found locally
+        current_system_ip = socket.gethostbyname(socket.gethostname())
         for system_ip, system_port in other_systems:
-            try:
-                other_server_socket = socket.socket(
-                    socket.AF_INET, socket.SOCK_STREAM)
-                other_server_socket.connect((system_ip, system_port))
-                other_server_socket.send(file_name.encode())
-                file_content = other_server_socket.recv(1024).decode()
-                file_content_with_info = f"File content from server {server_info}:\n{file_content}"
-                client_socket.send(file_content_with_info.encode())
-                # Save the file received from other system locally
-                save_file(file_path, file_content)
-                other_server_socket.close()
-                return
-            except Exception as e:
-                print(
-                    f"Failed to retrieve file from {system_ip}:{system_port}: {e}")
+            if system_ip != current_system_ip:  # Check if the IP address is not equal to the current server's IP address
+                try:
+                    other_server_socket = socket.socket(
+                        socket.AF_INET, socket.SOCK_STREAM)
+                    other_server_socket.connect((system_ip, system_port))
+                    other_server_socket.send(file_name.encode())
+                    file_content = other_server_socket.recv(1024).decode()
+                    file_content_with_info = f"File content from server {server_info}:\n{file_content}"
+                    client_socket.send(file_content_with_info.encode())
+                    # Save the file received from other system locally
+                    save_file(file_path, file_content)
+                    other_server_socket.close()
+                    return
+                except Exception as e:
+                    print(
+                        f"Failed to retrieve file from {system_ip}:{system_port}: {e}")
 
         client_socket.send("File not found.".encode())
 
